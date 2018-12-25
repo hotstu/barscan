36b9f9eb15dca72940387194162ee9f318c5ca08
hglf <hgleifeng@foxmail.com>
2018-12-25 15:56:32 +0800

    init

---
+++ barscaner/src/main/java/github/hotstu/lib/barscaner/CaptureActivity.java
@@ -0,0 +1,307 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package github.hotstu.lib.barscaner;
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.zxing.Result;
+import com.google.zxing.ResultMetadataType;
+import github.hotstu.lib.barscaner.R;
+
+import github.hotstu.lib.barscaner.camera.CameraManager;
+import github.hotstu.lib.barscaner.result.ResultHandler;
+import github.hotstu.lib.barscaner.result.ResultHandlerFactory;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.EnumSet;
+
+/**
+ * This activity opens the camera and does the actual scanning on a background thread. It draws a
+ * viewfinder to help the user place the barcode correctly, shows feedback as the image processing
+ * is happening, and then overlays the results when a scan is successful.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ * @author Sean Owen
+ */
+public final class CaptureActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private static final String TAG = CaptureActivity.class.getSimpleName();
+
+    private static final Collection<ResultMetadataType> DISPLAYABLE_METADATA_TYPES =
+            EnumSet.of(ResultMetadataType.ISSUE_NUMBER,
+                    ResultMetadataType.SUGGESTED_PRICE,
+                    ResultMetadataType.ERROR_CORRECTION_LEVEL,
+                    ResultMetadataType.POSSIBLE_COUNTRY);
+
+    private CameraManager cameraManager;
+    private CaptureActivityHandler handler;
+    private ViewfinderView viewfinderView;
+    private TextView statusView;
+    private boolean hasSurface;
+    private InactivityTimer inactivityTimer;
+    private BeepManager beepManager;
+    private AmbientLightManager ambientLightManager;
+
+    ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.capture);
+        initTopBar();
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
+        beepManager = new BeepManager(this);
+        ambientLightManager = new AmbientLightManager(this);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+    }
+
+    /**
+     * overide this method if you want change title
+     */
+    protected void initTopBar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (android.R.id.home == item.getItemId()) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // CameraManager must be initialized here, not in onCreate(). This is necessary because we don't
+        // want to open the camera driver and measure the screen size if we're going to show the help on
+        // first launch. That led to bugs where the scanning rectangle was the wrong size and partially
+        // off screen.
+        cameraManager = new CameraManager(getApplication());
+
+        viewfinderView = findViewById(R.id.viewfinder_view);
+        viewfinderView.setCameraManager(cameraManager);
+
+        statusView = findViewById(R.id.status_view);
+
+        handler = null;
+
+        resetStatusView();
+        beepManager.updatePrefs();
+        ambientLightManager.start(cameraManager);
+        inactivityTimer.onResume();
+
+        SurfaceView surfaceView =  findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            // The activity was paused but not stopped, so the surface still exists. Therefore
+            // surfaceCreated() won't be called, so init the camera here.
+            initCamera(surfaceHolder);
+        } else {
+            // Install the callback and wait for surfaceCreated() to init the camera.
+            surfaceHolder.addCallback(this);
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        inactivityTimer.onPause();
+        ambientLightManager.stop();
+        beepManager.close();
+        cameraManager.closeDriver();
+        //historyManager = null; // Keep for onActivityResult
+        if (!hasSurface) {
+            SurfaceView surfaceView =  findViewById(R.id.preview_view);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.removeCallback(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        inactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                setResult(RESULT_CANCELED);
+                finish();
+                return true;
+            case KeyEvent.KEYCODE_FOCUS:
+            case KeyEvent.KEYCODE_CAMERA:
+                // Handle these events so they don't launch the Camera app
+                return true;
+            // Use volume up/down to turn on light
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                cameraManager.setTorch(false);
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                cameraManager.setTorch(true);
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder == null) {
+            Log.e(TAG, "*** WARNING *** surfaceCreated() gave us a null surface!");
+        }
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // do nothing
+    }
+
+    /**
+     * A valid barcode has been found, so give an indication of success and show the results.
+     *
+     * @param rawResult   The contents of the barcode.
+     * @param scaleFactor amount by which thumbnail was scaled
+     * @param barcode     A greyscale bitmap of the camera data which was decoded.
+     */
+    public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        inactivityTimer.onActivity();
+        ResultHandler resultHandler = ResultHandlerFactory.makeResultHandler(this, rawResult);
+        beepManager.playBeepSoundAndVibrate();
+        handleDecodeInternally(rawResult, resultHandler, barcode);
+
+    }
+
+
+    // Put up our own UI for how to handle the decoded contents.
+    private void handleDecodeInternally(Result rawResult, ResultHandler resultHandler, Bitmap barcode) {
+        Log.d(TAG, rawResult.getText());
+        onDecodeSuccess( rawResult,  resultHandler,  barcode);
+    }
+
+    /**
+     * override this method on you own busisness
+     * 复写这个方法实现你的业务
+     * @param rawResult
+     * @param resultHandler
+     * @param barcode
+     */
+    protected  void onDecodeSuccess(Result rawResult, ResultHandler resultHandler, Bitmap barcode){};
+
+
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+        if (cameraManager.isOpen()) {
+            Log.w(TAG, "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
+        try {
+            cameraManager.openDriver(surfaceHolder);
+            // Creating the handler starts the preview, which can also throw a RuntimeException.
+            if (handler == null) {
+                handler = new CaptureActivityHandler(this,
+                        DecodeFormatManager.QR_CODE_FORMATS, null, null, cameraManager);
+            }
+        } catch (IOException ioe) {
+            Log.w(TAG, ioe);
+            displayFrameworkBugMessageAndExit();
+        } catch (RuntimeException e) {
+            // Barcode Scanner has seen crashes in the wild of this variety:
+            // java.?lang.?RuntimeException: Fail to connect to camera service
+            Log.w(TAG, "Unexpected error initializing camera", e);
+            displayFrameworkBugMessageAndExit();
+        }
+    }
+
+    private void displayFrameworkBugMessageAndExit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setMessage(getString(R.string.msg_camera_framework_bug));
+        builder.setPositiveButton(R.string.button_ok, new FinishListener(this));
+        builder.setOnCancelListener(new FinishListener(this));
+        builder.show();
+    }
+
+    public void restartPreviewAfterDelay(long delayMS) {
+        if (handler != null) {
+            handler.sendEmptyMessageDelayed(R.id.restart_preview, delayMS);
+        }
+        resetStatusView();
+    }
+
+    private void resetStatusView() {
+        statusView.setText(R.string.msg_default_status);
+        statusView.setVisibility(View.VISIBLE);
+        viewfinderView.setVisibility(View.VISIBLE);
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+}
---
+++ barscaner/src/main/java/github/hotstu/lib/barscaner/ViewfinderView.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package github.hotstu.lib.barscaner;
+
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.google.zxing.ResultPoint;
+import github.hotstu.lib.barscaner.R;
+
+import github.hotstu.lib.barscaner.camera.CameraManager;
+
+/**
+ * This view is overlaid on top of the camera preview. It adds the viewfinder rectangle and partial
+ * transparency outside it, as well as the laser scanner animation and result points.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+public final class ViewfinderView extends View {
+
+    private static final int[] SCANNER_ALPHA = {0, 64, 128, 192, 255, 192, 128, 64};
+
+    private static final int MIN_FRAME_WIDTH = 240;
+    private static final int MIN_FRAME_HEIGHT = 240;
+    private static final int MAX_FRAME_WIDTH = 1200; // = 5/8 * 1920
+    private static final int MAX_FRAME_HEIGHT = 675; // = 5/8 * 1080
+
+    private CameraManager cameraManager;
+    private final Paint paint;
+    private final int maskColor;
+    private final int laserColor;
+    private int scannerAlpha;
+    private final ValueAnimator mAnim;
+    private final RectF ovalRect;
+
+    // This constructor is used when the class is built from an XML resource.
+    public ViewfinderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Initialize these once for performance rather than calling them every time in onDraw().
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Resources resources = getResources();
+        maskColor = resources.getColor(R.color.viewfinder_mask);
+        laserColor = resources.getColor(R.color.viewfinder_laser);
+        scannerAlpha = 0;
+        mAnim = ValueAnimator.ofFloat(0, 1f);
+        mAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        mAnim.setRepeatMode(ValueAnimator.RESTART);
+        mAnim.setRepeatCount(ValueAnimator.INFINITE);
+        mAnim.setDuration(2000);
+        ovalRect = new RectF();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mAnim.start();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mAnim.cancel();
+        super.onDetachedFromWindow();
+    }
+
+    public void setCameraManager(CameraManager cameraManager) {
+        this.cameraManager = cameraManager;
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (cameraManager == null) {
+            return; // not ready yet, early draw before done configuring
+        }
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        int SqureWidth = findDesiredDimensionInRange(width, MIN_FRAME_WIDTH, MAX_FRAME_WIDTH);
+        int SqureHeight = findDesiredDimensionInRange(height, MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
+
+        int leftOffset = (width - SqureWidth) / 2;
+        int topOffset = (height - SqureHeight) / 2;
+
+        paint.setColor(maskColor);
+        canvas.drawRect(0, 0, width, topOffset, paint);
+        canvas.drawRect(0, topOffset, leftOffset, topOffset + SqureHeight + 1, paint);
+        canvas.drawRect(leftOffset + SqureWidth + 1, topOffset, width, topOffset + SqureHeight + 1, paint);
+        canvas.drawRect(0, topOffset + SqureHeight + 1, width, height, paint);
+//        Rect framingRect = cameraManager.getFramingRect();
+//        Paint.Style style = paint.getStyle();
+//        if (framingRect != null) {
+//            paint.setStyle(Paint.Style.STROKE);
+//            canvas.drawRect(framingRect, paint);
+//        }
+//        Rect pframingRect = cameraManager.getFramingRectInPreview();
+//        if (pframingRect != null) {
+//            paint.setColor(Color.WHITE);
+//            paint.setStyle(Paint.Style.STROKE);
+//            canvas.drawRect(pframingRect, paint);
+//        }
+//
+//        paint.setStyle(style);
+
+        // Draw a red "laser scanner" line through the middle to show decoding is active
+        paint.setColor(laserColor);
+        paint.setAlpha(SCANNER_ALPHA[scannerAlpha]);
+        scannerAlpha = (scannerAlpha + 1) % SCANNER_ALPHA.length;
+        float fragtion = mAnim.getAnimatedFraction();
+        float middle =  (SqureHeight*fragtion + topOffset);
+        ovalRect.set(leftOffset + 2, middle - 2, leftOffset + SqureWidth - 1, middle + 2);
+        canvas.drawOval(ovalRect, paint);
+
+        postInvalidateOnAnimation();
+
+    }
+
+    private static int findDesiredDimensionInRange(int resolution, int hardMin, int hardMax) {
+        int dim = 5 * resolution / 8; // Target 5/8 of each dimension
+        if (dim < hardMin) {
+            return hardMin;
+        }
+        if (dim > hardMax) {
+            return hardMax;
+        }
+        return dim;
+    }
+
+    public void drawViewfinder() {
+        invalidate();
+    }
+
+
+    public void addPossibleResultPoint(ResultPoint point) {
+    }
+
+}
